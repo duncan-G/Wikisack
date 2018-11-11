672b34945db607eb4b04a259c5aa7251434d2ac4
Duncan <duncangichimu@gmail.com>
2018-11-10 21:23:27 -0800

    add comments

--- models/db.js
+++ models/db.js
@@ -1,6 +1,16 @@
 const Sequelize = require('sequelize');
 const config = require('./.db.config');
 
+/*
+  config = {
+    username: 'username',
+    password: 'password',
+    database: 'wikistack',
+    host: 'localhost',
+    port: 5432,
+  };
+*/
+
 config.databaseUrl = function() {
   return `postgres://${this.username}:${this.password}@${this.host}:${this.port}/${this.database}`;
 };
