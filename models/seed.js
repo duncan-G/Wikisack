57baee0d2a8138055aab08ec2dab83ae6ac953ae
Duncan <duncangichimu@gmail.com>
2018-11-13 12:05:27 -0800

    update seed file

--- models/seed.js
+++ models/seed.js
@@ -2,28 +2,36 @@ const models = require('./models');
 const faker = require('faker');
 faker.seed(3523);
 
+const tags = Array(15).fill(null).map(_ => faker.random.word);
+const getTags(tags, size) => {
+  var shuffled = arr.slice(0), i = arr.length, temp, index;
+  while (i--) {
+      index = Math.floor((i + 1) * Math.random());
+      temp = shuffled[index];
+      shuffled[index] = shuffled[i];
+      shuffled[i] = temp;
+  }
+  return shuffled.slice(0, size);
+}
 const pageData = Array(50)
   .fill(null)
   .map(_ => {
     return {
       title: faker.lorem.words,
       content: faker.lorem.paragraphs,
-      status: ['open', 'closed'][Math.floor(Math.random() * 2)]
+      status: ['open', 'closed'][Math.floor(Math.random() * 2)],
+      tags: faker.random.words.split(' ')
     };
   });
 
-const userData = Array(10).fill(null).map(_ => {
-  return {
-    name : faker.name.findName();
-    email : faker.internet.email();
-  }
-})
-
-const tagData = [
-  { size: 20, shaded: false },
-  { size: 40, shaded: true },
-  { size: 10, shaded: false }
-];
+const userData = Array(10)
+  .fill(null)
+  .map(_ => {
+    return {
+      name: faker.name.findName(),
+      email: faker.internet.email()
+    };
+  });
 
 db.sync({ force: true })
   .then(() => {
@@ -31,13 +39,12 @@ db.sync({ force: true })
     // `returning: true` is a postgres option returning representations of
     // the new data. Otherwise we get back a success report, not instances.
     return Promise.all([
-      Vegetable.bulkCreate(vegetableData, { returning: true }),
-      Gardener.bulkCreate(gardenerData, { returning: true }),
-      Plot.bulkCreate(plotData, { returning: true })
+      models.Page.bulkCreate(pageData, { returning: true }),
+      models.User.bulkCreate(userData, { returning: true }),
     ]);
   })
   .then(insertedData => {
-    const [vegetables, gardeners, plots] = insertedData;
+    const [pages, users] = insertedData;
     const [carrot, tomato, pepper] = vegetables;
     const [mcgregor, hashimoto, giancarlo] = gardeners;
     const [mcgregorPlot, hashimotoPlot, giancarloPlot] = plots;
